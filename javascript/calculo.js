853f652f8be7c1902371f7e7acfbba060239c1fe
EricLamounier <eric_lamounier@outlook.com>
2023-04-22 16:55:59 -0300

    commit

--- javascript/calculo.js
+++ javascript/calculo.js
@@ -1,20 +1,20 @@
 import { bin_dec } from '../javascript/binary.js'
 import { dec_bin } from '../javascript/decimal.js'
-let n1 = document.getElementById('n1').value;
-let n2 = document.getElementById('n2').value;
+let n1 = document.getElementById('n1');
+let n2 = document.getElementById('n2');
 
 
 let bttnCalc = document.getElementById('calcular');
 
 bttnCalc.addEventListener('click', () => {
-
+    
     let sum = document.getElementById('soma');
     let sub = document.getElementById('subtracao');
     let mul = document.getElementById('multiplicacao');
     let div = document.getElementById('divisao');
 
-    let decN1 = bin_dec(n1);
-    let decN2 = bin_dec(n2);
+    let decN1 = bin_dec(n1.value);
+    let decN2 = bin_dec(n2.value);
     let aux;
 
     //soma
